7ff9e6cf534b7ed7d212a3718fa84453e747a417
ekunish <ekunish@gmail.com>
2020-07-12 15:41:05 +0900

    Update soup.c
    change arajiru to acqua_pazza

--- soup.c
+++ soup.c
@@ -5,39 +5,49 @@ dish soup(){
     gram sea_bream = 300;
     gram salt_0 = 20;
     gram clam = 200;
-    gram salt_1 = 20;
+    gram salt_0 = 20;
     gram water_0 = 400;
-    gram water_1 = 400;
-    gram green_onion = 5;
-    gram ginger = 10;
-    gram miso = 15;
-    gram shichimi = 0.2;
+    gram cherry_tomato = 160;
+    gram parsley = 5;
+    gram garlic = 10;
+    gram olive_oil = 15;
+    gram white_wine = 30;
+    gram water_1 = 100;
+    gram pepper = 0.2;
 
     soak(clam, [water_0, salt_0]);
 
     season(sea_bream, salt_1);
 
-    cut(green_onion);
-    cut(ginger);
+    cut(cherry_tomato);
+    cut(garlic);
 
     cookware pot;
     pot.heat.start("middle");
 
-    pot.insert(water_1);
+    pot.insert(olive_oil);
+    pot.insert(garlic);
+    while(garlic.status != "brown"){}
+
+
     pot.insert(sea_bream);
+    while(sea_bream.status != "brown"){}
+
     pot.insert(clam);
+    pot.insert(cherry_tomato);
+    pot.insert(white_wine);
+    pot.insert(water_1);
 
     sleep(5);
     pot.heat.stop();
 
-    pot.insert(ginger);
-    pot.insert(miso);
-    pot.insert(green_onion);
+    cut(parsley);
+    pot.insert(parsley);
 
-    dish bowl;
-    bowl = pot.output();
+    dish plate;
+    plate = pot.output();
 
-    bowl.insert(shichimi);
+    plate.insert(pepper);
 
-    return bowl;
+    return plate;
 }
